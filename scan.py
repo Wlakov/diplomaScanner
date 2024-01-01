58facf3b79180949db093f7f3532edba63ed5210
kali <koval0310@gmail.com>
2024-01-01 10:07:23 -0500

    update

--- scan.py
+++ scan.py
@@ -1,28 +1,130 @@
-import nmap
+import socket
+import os
+import signal
+import time
+import threading
+import sys
+import subprocess
+from queue import Queue
+from datetime import datetime
 
 
-def scan_network(ip_range):
-    nm = nmap.PortScanner()
-    nm.scan(hosts=ip_range,
-            arguments='-sV -p-')  # -sV для виведення інформації про версії, -p- для сканування всіх портів
+# Main Function
+def main():
+    socket.setdefaulttimeout(0.30)
+    print_lock = threading.Lock()
+    discovered_ports = []
 
-    for host in nm.all_hosts():
-        print(f"Host: {host}")
-        print(f"  Name: {nm[host].hostname()}")
+    # Welcome Banner
+    print("-" * 60)
+    print("                      Network Scanner          ")
+    print("                       Version 1.0.0                    ")
+    print("                   A project by Vladyslav K.               ")
+    print("-" * 60)
+    time.sleep(1)
+    target = input("Enter your target IP address or URL here: ")
+    error = "Invalid Input"
+    try:
+        t_ip = socket.gethostbyname(target)
+    except (UnboundLocalError, socket.gaierror):
+        print("\n[-]Invalid format. Please use a correct IP or web address[-]\n")
+        sys.exit()
+    # Banner
+    print("-" * 60)
+    print("Scanning target " + t_ip)
+    print("Time started: " + str(datetime.now()))
+    print("-" * 60)
+    t1 = datetime.now()
 
-        if 'osclass' in nm[host]:
-            for osclass in nm[host]['osclass']:
-                print(f"  OS Class: {osclass['osclasstype']} - {osclass['osfamily']} {osclass['osgen']}")
+    def portscan(port):
 
-        for proto in nm[host].all_protocols():
-            print(f"  Protocol: {proto}")
-            ports = nm[host][proto].keys()
-            for port in ports:
-                port_info = nm[host][proto][port]
-                print(
-                    f"    Port: {port} - State: {port_info['state']} - Service: {port_info['name']} - Version: {port_info['product']} {port_info['version']}")
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        try:
+            portx = s.connect((t_ip, port))
+            with print_lock:
+                print("Port {} is open".format(port))
+                discovered_ports.append(str(port))
+            portx.close()
+
+        except (ConnectionRefusedError, AttributeError, OSError):
+            pass
+
+    def threader():
+        while True:
+            worker = q.get()
+            portscan(worker)
+            q.task_done()
+
+    q = Queue()
+
+    # startTime = time.time()
+
+    for x in range(200):
+        t = threading.Thread(target=threader)
+        t.daemon = True
+        t.start()
+
+    for worker in range(1, 65536):
+        q.put(worker)
+
+    q.join()
+
+    t2 = datetime.now()
+    total = t2 - t1
+    print("Port scan completed in " + str(total))
+    print("-" * 60)
+    print("Recommends the following network scan:")
+    print("*" * 60)
+    print("nmap -p{ports} -sV {ip}".format(ports=",".join(discovered_ports), ip=target))
+    print("*" * 60)
+    nmap = "nmap -p{ports} -sV {ip}".format(ports=",".join(discovered_ports), ip=target)
+    t3 = datetime.now()
+    total1 = t3 - t1
+
+    # Nmap Integration (in progress)
+
+    def automate():
+        choice = '0'
+        while choice == '0':
+            print("Would you like to run Nmap or quit to terminal?")
+            print("-" * 60)
+            print("1 = Run suggested Nmap scan")
+            print("2 = Run another scan")
+            print("3 = Exit to terminal")
+            print("-" * 60)
+            choice = input("Option Selection: ")
+            if choice == "1":
+                try:
+                    print(nmap)
+                    # os.mkdir(target)
+                    # os.chdir(target)
+                    os.system(nmap)
+                    # convert = "xsltproc "+target+".xml -o "+target+".html"
+                    # os.system(convert)
+                    t3 = datetime.now()
+                    total1 = t3 - t1
+                    print("-" * 60)
+                    print("Combined scan completed in " + str(total1))
+                    print("Press enter to quit...")
+                    input()
+                except FileExistsError as e:
+                    print(e)
+                    exit()
+            elif choice == "2":
+                main()
+            elif choice == "3":
+                sys.exit()
+            else:
+                print("Please make a valid selection")
+                automate()
+
+    automate()
 
 
-ip_range = '192.168.1.1-255'  # Замініть на ваш діапазон IP
-
-scan_network(ip_range)
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\nGoodbye!")
+        quit()
