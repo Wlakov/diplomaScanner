4ea5d6b6adbd0f06e0346cb5304f4a19f0f3caf7
kali <koval0310@gmail.com>
2024-01-10 08:14:46 -0500

    update

--- regularFile.py
+++ regularFile.py
@@ -1,4 +1,16 @@
 import nmap
+import mysql.connector
+
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="kali",
+    database="Testip"
+)
+
+mycursor = mydb.cursor()
+
 
 def nmap_version_scan(target_ip):
     nm = nmap.PortScanner()
@@ -10,26 +22,44 @@ def nmap_version_scan(target_ip):
         for proto in nm[host].all_protocols():
             ports = nm[host][proto].keys()
             for port in ports:
+                sql = "INSERT  INTO scan_results (ip, port, protocol, service, product, version) VALUES (%s,%s,%s,%s,%s,%s)"
                 service = nm[host][proto][port]
+                values = host, port, proto, service['name'], service['product'], service['version']
+
+
+
+                
+
+
+
+
+
+
+
+                mycursor.execute(sql, values)
+                mydb.commit()
+
                 scan_results.append({
                     'ip': host,
                     'port': port,
                     'protocol': proto,
                     'service_name': service['name'],
                     'service_product': service['product'],
-                    'service_version': service['version'],
-                    'service_extrainfo': service['extrainfo']
+                    'service_version': service['version']
                 })
 
     return scan_results
 
-# Визначаємо IP-адресу для сканування
-target_ip = "45.44.151.148"
 
-# Викликаємо функцію сканування та виводимо результат
-scan_results = nmap_version_scan(target_ip)
-for result in scan_results:
-    print(f"IP: {result['ip']}, Port: {result['port']}, Protocol: {result['protocol']}")
-    print(f"Service: {result['service_name']}, Product: {result['service_product']}")
-    print(f"Version: {result['service_version']}, Extra Info: {result['service_extrainfo']}")
-    print("-" * 30)
+if __name__ == '__main__':
+    target_ip = "45.44.151.148"
+    scan_results = nmap_version_scan(target_ip)
+    for result in scan_results:
+        print(f"IP: {result['ip']}, Port: {result['port']}, Protocol: {result['protocol']}")
+        print(f"Service: {result['service_name']}, Product: {result['service_product']}")
+        print(f"Version: {result['service_version']}")
+        print("-" * 30)
+
+
+
+# 45.44.151.148
